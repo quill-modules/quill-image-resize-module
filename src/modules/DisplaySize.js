3ad0bf1e3dffcda8dc95b325c1d11d62c23e3c50
zzxming <zzxming77@163.com>
2024-07-12 18:16:23 +0800

    fix: display box wrong position when out of box

--- src/modules/DisplaySize.js
+++ src/modules/DisplaySize.js
@@ -1,56 +1,50 @@
 import { BaseModule } from './BaseModule';
 
 export class DisplaySize extends BaseModule {
-    onCreate = () => {
-        // Create the container to hold the size display
-        this.display = document.createElement('div');
+  onCreate = () => {
+    // Create the container to hold the size display
+    this.display = document.createElement('div');
 
-        // Apply styles
-        Object.assign(this.display.style, this.options.displayStyles);
+    // Apply styles
+    Object.assign(this.display.style, this.options.displayStyles);
 
-        // Attach it
-        this.overlay.appendChild(this.display);
-    };
+    // Attach it
+    this.overlay.appendChild(this.display);
+  };
 
-    onDestroy = () => {};
+  onDestroy = () => {};
 
-    onUpdate = () => {
-        if (!this.display || !this.img) {
-            return;
-        }
+  onUpdate = () => {
+    if (!this.display || !this.img) {
+      return;
+    }
 
-        const size = this.getCurrentSize();
-        this.display.innerHTML = size.join(' &times; ');
-        if (size[0] > 120 && size[1] > 30) {
-            // position on top of image
-            Object.assign(this.display.style, {
-                right: '4px',
-                bottom: '4px',
-                left: 'auto',
-            });
-        }
-        else if (this.img.style.float == 'right') {
-			// position off bottom left
-            const dispRect = this.display.getBoundingClientRect();
-            Object.assign(this.display.style, {
-                right: 'auto',
-                bottom: `-${dispRect.height + 4}px`,
-                left: `-${dispRect.width + 4}px`,
-            });
-        }
-        else {
-            // position off bottom right
-            const dispRect = this.display.getBoundingClientRect();
-            Object.assign(this.display.style, {
-                right: `-${dispRect.width + 4}px`,
-                bottom: `-${dispRect.height + 4}px`,
-                left: 'auto',
-            });
-        }
-    };
+    const size = this.getCurrentSize();
+    this.display.innerHTML = size.join(' &times; ');
+    if (size[0] > 120 && size[1] > 40) {
+      // position on top of image
+      Object.assign(this.display.style, {
+        right: '4px',
+        bottom: '4px',
+        left: null,
+        transform: null,
+      });
+    } else if (this.img.style.float == 'right') {
+      // position off bottom left
+      Object.assign(this.display.style, {
+        right: null,
+        bottom: null,
+        transform: `translate(calc(-100% - 4px), ${this.img.height}px)`,
+      });
+    } else {
+      // position off bottom right
+      Object.assign(this.display.style, {
+        left: null,
+        bottom: null,
+        transform: `translate(calc(100% + 4px), ${this.img.height}px)`,
+      });
+    }
+  };
 
-    getCurrentSize = () => [
-        this.img.width,
-        Math.round((this.img.width / this.img.naturalWidth) * this.img.naturalHeight),
-    ];
+  getCurrentSize = () => [this.img.width, this.img.height];
 }
